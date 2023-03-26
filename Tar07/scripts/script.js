e5375a13359925cbc3de801083ea548a67164fde
Senorita-glez <77082096+Senorita-glez@users.noreply.github.com>
2023-03-26 17:42:50 -0600

    Update script.js

--- Tar07/scripts/script.js
+++ Tar07/scripts/script.js
@@ -3,16 +3,19 @@ var mediaqueryList = window.matchMedia("(max-width: 500px)");
 var y = document.getElementById("links");
 function myFunction() {
   var logo = document.getElementById("logo");
+  var nav = document.getElementById("navi");
   if(mediaqueryList.matches) {
       if (y.style.display === "flex") {
         y.style.display = "none";
         y.style.flexDirection = "row";  
         logo.style.display = "block"  
+        navi.style.backgroundColor = "initial";
       } else {
         y.style.display = "flex";
         y.style.flexDirection = "column";
         y.style.zIndex="5"
         logo.style.display = "none";
+        navi.style.backgroundColor = "rgba(9, 40, 17, 0.66)";
       }
   }
   else{
@@ -29,7 +32,7 @@ function go(){
   }  
   else{
     y.style.display = "initial";
+    navi.style.backgroundColor = "initial";
   }
 }
-
 
