ee98a4bf89f795db5d39a9fe6d4cf1139900a3db
Senorita-glez <77082096+Senorita-glez@users.noreply.github.com>
2023-03-26 17:26:53 -0600

    Add files via upload

---
+++ Tar07/scripts/script.js
@@ -0,0 +1,35 @@
+
+var mediaqueryList = window.matchMedia("(max-width: 500px)");
+var y = document.getElementById("links");
+function myFunction() {
+  var logo = document.getElementById("logo");
+  if(mediaqueryList.matches) {
+      if (y.style.display === "flex") {
+        y.style.display = "none";
+        y.style.flexDirection = "row";  
+        logo.style.display = "block"  
+      } else {
+        y.style.display = "flex";
+        y.style.flexDirection = "column";
+        y.style.zIndex="5"
+        logo.style.display = "none";
+      }
+  }
+  else{
+    y.style.display = "initial";
+  }
+}
+
+go();
+window.addEventListener('resize', go);
+
+function go(){
+  if(mediaqueryList.matches){
+    y.style.display = "none";
+  }  
+  else{
+    y.style.display = "initial";
+  }
+}
+
+
